5e90f3df85052c75da0ce81f4e0a2daf2b51e7bd
Brian Mingus <reflection@gmail.com>
2025-08-30 18:57:02 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-
-from setuptools import setup, Extension
+from setuptools import setup
 from Cython.Build import cythonize
-import os
 
-extra_compile_args = [
-    "-O3","-march=native","-mtune=native","-fno-plt","-pipe",
-    "-flto","-fomit-frame-pointer","-funroll-loops",
-]
-extra_link_args = ["-flto"]
-
-extensions = [
-    Extension(
-        "proxy",
-        sources=["proxy.pyx"],
-        libraries=[],
-        extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args,
-    )
-]
-
-setup(
-    name="proxy_fast",
-    version="0.1.0",
-    ext_modules=cythonize(extensions, language_level=3, annotate=False, nthreads=os.cpu_count() or 4),
-)
+setup(ext_modules=cythonize("proxy.pyx", language_level="3"))
